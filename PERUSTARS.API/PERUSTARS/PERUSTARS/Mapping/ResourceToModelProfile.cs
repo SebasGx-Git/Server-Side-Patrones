373e6e9fe30a8cbf6b62d8873960d0b0eab0be18
Gabriel Vasquez Guerra <gabriel.vasquez391@hotmail.com>
2021-06-18 10:56:48 -0500

    Adding Persistence, Services, Resources and configuring Program.cs and Startup.cs

---
+++ PERUSTARS.API/PERUSTARS/PERUSTARS/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PERUSTARS.Domain.Models;
+using PERUSTARS.Resources; 
+
+namespace PERUSTARS.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile() {
+            CreateMap<SaveArtistResource, Artist>();
+            CreateMap<SavePersonResource, Person>();
+            CreateMap<SaveArtworkResource, Artwork>();
+            CreateMap<SaveEventResource, Event>();
+            CreateMap<SaveClaimTicketResource, ClaimTicket>();
+            CreateMap<SaveEventAssistanceResource, EventAssistance>();
+            CreateMap<SaveHobbyistResource, Hobbyist>();
+        }
+    }
+}
--- PERUSTARS.API/PERUSTARS/PERUSTARS/Startup.cs
+++ PERUSTARS.API/PERUSTARS/PERUSTARS/Startup.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PERUSTARS.Domain.Persistence.Contexts;
+using PERUSTARS.Domain.Persistence.Repositories;
+using PERUSTARS.Domain.Services;
+using PERUSTARS.Persistence.Repositories;
+using PERUSTARS.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +32,53 @@ namespace PERUSTARS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            //Database
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Dependency Injection Configuration
+
+            services.AddScoped<IArtistRepository, ArtistRepository>();
+            services.AddScoped<IArtworkRepository, ArtworkRepository>();
+            services.AddScoped<IHobbyistRepository, HobbyistRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IClaimTicketRepository, ClaimTicketRepository>();
+            services.AddScoped<IInterestRepository, InterestRepository>();
+            services.AddScoped<IFavoriteArtworkRepository, FavoriteArtworkRepository>();
+            services.AddScoped<ISpecialtyRepository, SpecialtyRepository>();
+            services.AddScoped<IFollowerRepository, FollowerRepository>();
+            services.AddScoped<IEventAssistanceRepository, EventAssistanceRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IArtworkService, ArtworkService>();
+            services.AddScoped<IArtistService, ArtistService>();
+            services.AddScoped<IHobbyistService, HobbyistService>();
+            services.AddScoped<ISpecialtyService, SpecialtyService>();
+            services.AddScoped<IFollowerService, FollowerService>();
+            services.AddScoped<IEventService, EventService>();
+            services.AddScoped<IInterestService, InterestService>();
+            services.AddScoped<IEventAssistanceService, EventAssistanceService>();
+            services.AddScoped<IFavoriteArtworkService, FavoriteArtworkService>();
+            services.AddScoped<IClaimTicketService, ClaimTicketService>();
+            services.AddScoped<ISpecialtyService, SpecialtyService>();
+
+
+            // Apply Endpoints Naming Convention
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            // AutoMapper Setup
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PERUSTARS", Version = "v1" });
+                c.EnableAnnotations();
             });
         }
 
