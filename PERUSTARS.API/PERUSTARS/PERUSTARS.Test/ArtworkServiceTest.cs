5cf0d1c10b9350675906dbc047d2f8364bda2ea5
Gabriel Vasquez Guerra <gabriel.vasquez391@hotmail.com>
2021-06-19 18:44:45 -0500

    added tests

---
+++ PERUSTARS.API/PERUSTARS/PERUSTARS.Test/ArtworkServiceTest.cs
@@ -0,0 +1,187 @@
+﻿using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using PERUSTARS.Domain.Models;
+using PERUSTARS.Domain.Services.Communications;
+using PERUSTARS.Domain.Persistence.Repositories;
+using PERUSTARS.Services;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Test
+{
+    class ArtworkServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        // GET BY ID
+        [Test]
+        public async Task GetByIdWhenValidArtworkReturnsArtwork()
+        {
+            // Arrange
+            var mockArtworkRepository = GetDefaultIArtworkRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockFavoriteArtworkRepository = GetDefaultFavoriteArtworkRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+            var artworkId = 1;
+            var artistId = 1;
+
+            Artwork artwork = new Artwork();
+            artwork.ArtworkId = artworkId;
+
+            mockArtistRepository.Setup(r => r.FindById(artistId))
+                .Returns(Task.FromResult<Artist>(new Artist()));
+
+            mockArtworkRepository.Setup(r => r.FindById(artworkId))
+                .Returns(Task.FromResult(artwork));
+
+            var service = new ArtworkService(mockArtworkRepository.Object, mockUnitOfWork.Object, mockFavoriteArtworkRepository.Object, mockArtistRepository.Object);
+
+            // Act
+            ArtworkResponse result = await service.GetByIdAndArtistIdAsync(artworkId, artistId);
+            var artworkResult = result.Resource;
+            // Assert
+            artworkResult.Should().Be(null);
+        }
+       
+        [Test]
+        public async Task GetByIdWhenNoArtworkReturnsArtworkNotFoundResponse()
+        {
+            // Arrange
+            var mockArtworkRepository = GetDefaultIArtworkRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockFavoriteArtworkRepository = GetDefaultFavoriteArtworkRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+            var artworkId = 1;
+            var artistId = 1;
+
+            mockArtistRepository.Setup(r => r.FindById(artistId))
+                .Returns(Task.FromResult<Artist>(new Artist()));
+
+            mockArtworkRepository.Setup(r => r.FindById(artworkId))
+                .Returns(Task.FromResult<Artwork>(null));
+
+            var service = new ArtworkService(mockArtworkRepository.Object, mockUnitOfWork.Object, mockFavoriteArtworkRepository.Object, mockArtistRepository.Object);
+
+            // Act
+            ArtworkResponse result = await service.GetByIdAndArtistIdAsync(artworkId, artistId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Artwork not found");
+        }
+
+
+        //REPITE TITULO DE OBRA
+
+        [Test]
+        public async Task GetIsSameTitleWhenArtistRepitTitle()
+        {
+            //Arrange
+            var mockArtworkRepository = GetDefaultIArtworkRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockFavoriteArtworkRepository = GetDefaultFavoriteArtworkRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+
+            Artist artist = new Artist();
+            artist.Id = 1;
+            artist.Firstname = "Sebastian";
+            artist.Lastname = "Gonzales";
+            artist.BrandName = "SebasGx";
+
+            Artwork artwork1 = new Artwork(); ;
+            artwork1.ArtworkId = 1;
+            artwork1.ArtistId = 1;
+            artwork1.ArtTitle = "hola";
+
+            Artwork artwork2 = new Artwork(); ;
+            artwork2.ArtworkId = 2;
+            artwork2.ArtistId = 1;
+            artwork2.ArtTitle = "adios";
+
+
+
+
+            mockArtworkRepository.Setup(r => r.isSameTitle("hola", 1))
+                  .Returns(Task.FromResult(true));
+
+            var service = new ArtworkService(mockArtworkRepository.Object, mockUnitOfWork.Object, mockFavoriteArtworkRepository.Object, mockArtistRepository.Object);
+
+
+            //Act
+
+
+            bool result = await service.isSameTitle("hola", 1);
+
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+
+        //NO REPITE TITULO DE OBRA
+
+        [Test]
+        public async Task GetIsSameTitleWhenArtistNOTRepitTitle()
+        {
+            //Arrange
+            var mockArtworkRepository = GetDefaultIArtworkRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockFavoriteArtworkRepository = GetDefaultFavoriteArtworkRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+
+            Artist artist = new Artist();
+            artist.Id = 1;
+            artist.Firstname = "Sebastian";
+            artist.Lastname = "Gonzales";
+            artist.BrandName = "SebasGx";
+
+            Artwork artwork1 = new Artwork(); ;
+            artwork1.ArtworkId = 1;
+            artwork1.ArtistId = 1;
+            artwork1.ArtTitle = "hola";
+
+            Artwork artwork2 = new Artwork(); ;
+            artwork2.ArtworkId = 2;
+            artwork2.ArtistId = 1;
+            artwork2.ArtTitle = "adios";
+
+
+
+
+            mockArtworkRepository.Setup(r => r.isSameTitle("titulo", 1))
+                  .Returns(Task.FromResult(false));
+
+            var service = new ArtworkService(mockArtworkRepository.Object, mockUnitOfWork.Object, mockFavoriteArtworkRepository.Object, mockArtistRepository.Object);
+
+
+            //Act
+
+
+            bool result = await service.isSameTitle("titulo", 1);
+
+
+            //Assert
+            Assert.AreEqual(result, false);
+        }
+
+        private Mock<IArtworkRepository> GetDefaultIArtworkRepositoryInstance()
+        {
+            return new Mock<IArtworkRepository>();
+        }
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+        private Mock<IFavoriteArtworkRepository> GetDefaultFavoriteArtworkRepositoryInstance()
+        {
+            return new Mock<IFavoriteArtworkRepository>();
+        }
+        private Mock<IArtistRepository> GetDefaultArtistRepositoryInstance()
+        {
+            return new Mock<IArtistRepository>();
+        }
+    }
+}
---
+++ PERUSTARS.API/PERUSTARS/PERUSTARS.Test/ClaimTicketServiceTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using PERUSTARS.Domain.Models;
+using PERUSTARS.Domain.Services.Communications;
+using PERUSTARS.Domain.Persistence.Repositories;
+using PERUSTARS.Services;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Test
+{
+    class ClaimTicketServiceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWhenValidClaimTicketReturnsClaimTicket()
+        {
+            var mockClaimTicketRepository = GetDefaultIClaimTicketRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockHobbyistRepository = GetDefaultHobbyistRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+            var hobbyistId = 1;
+            var artistId = 2;
+            var claimTicketId = 1;
+            ClaimTicket claimTicket = new ClaimTicket();
+            claimTicket.ClaimId = claimTicketId;
+            mockClaimTicketRepository.Setup(r => r.FindByIdAndPersonId(claimTicketId, hobbyistId))
+                .Returns(Task.FromResult(claimTicket));
+
+            var service = new ClaimTicketService(mockClaimTicketRepository.Object, mockUnitOfWork.Object, mockArtistRepository.Object, mockHobbyistRepository.Object);
+
+            // Act
+            ClaimTicketResponse result = await service.GetByIdAndPersonIdAsync(claimTicketId, hobbyistId);
+            var claimTicketResult = result.Resource;
+            // Assert
+            claimTicketResult.Should().Be(null);
+        }
+        [Test]
+        public async Task GetByIdAsyncWhenNoClaimTicketReturnsClaimTicketNotFoundResponse()
+        {
+            // Arrange
+            var mockClaimTicketRepository = GetDefaultIClaimTicketRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var mockHobbyistRepository = GetDefaultHobbyistRepositoryInstance();
+            var mockArtistRepository = GetDefaultArtistRepositoryInstance();
+            var hobbyistId = 1;
+            var artistId = 2;
+            var claimTicketId = 1;
+
+            mockHobbyistRepository.Setup(r => r.FindById(hobbyistId))
+                .Returns(Task.FromResult(new Hobbyist()));
+
+            mockClaimTicketRepository.Setup(r => r.FindByIdAndPersonId(claimTicketId, hobbyistId))
+                .Returns(Task.FromResult<ClaimTicket>(null));
+
+            var service = new ClaimTicketService(mockClaimTicketRepository.Object, mockUnitOfWork.Object, mockArtistRepository.Object, mockHobbyistRepository.Object);
+
+            // Act
+            ClaimTicketResponse result = await service.GetByIdAndPersonIdAsync(claimTicketId, hobbyistId);
+            var message = result.Message;
+            // Assert
+            message.Should().Be($"Claim Ticket not found by Person with Id: {hobbyistId}");
+        }
+        private Mock<IClaimTicketRepository> GetDefaultIClaimTicketRepositoryInstance()
+        {
+            return new Mock<IClaimTicketRepository>();
+        }
+        private Mock<IArtistRepository> GetDefaultArtistRepositoryInstance()
+        {
+            return new Mock<IArtistRepository>();
+        }
+        private Mock<IHobbyistRepository> GetDefaultHobbyistRepositoryInstance()
+        {
+            return new Mock<IHobbyistRepository>();
+        }
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+    }
+}
